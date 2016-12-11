6186f964e2f8c8708ecfb3a85dac0653a7431f97
IhsanTang <IhsanTang@163.com>
2016-12-11 20:12:51 +0800

    初始化

--- api-order/crap-blog-service/src/main/java/cn/crap/blog/service/ISettingService.java
+++
@@ -1,9 +0,0 @@
-package cn.crap.blog.service;
-
-import java.util.List;
-
-import cn.crap.blog.domain.dao.Setting;
-
-public interface ISettingService {
-List<Setting> selectByIds(List<Long> ids);
-}
